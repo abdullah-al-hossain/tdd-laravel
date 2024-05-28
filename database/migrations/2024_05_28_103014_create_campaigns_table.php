a6d214ee662d6b18cd4dbc87d2adba1f7c0984c6
abdullah-al-hossain <abdullahcuet14cse@gmail.com>
2024-05-28 18:27:22 +0600

    test for checking if all campaigns has the keys that are needed

--- database/migrations/2024_05_28_103014_create_campaigns_table.php
+++ database/migrations/2024_05_28_103014_create_campaigns_table.php
@@ -13,7 +13,23 @@ return new class extends Migration
     {
         Schema::create('campaigns', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('subject');
+            $table->text('content');
+            $table->boolean('status')->default(0);
+            $table->timestamp('scheduled_at')->nullable();
+            $table->unsignedInteger('total_recipients')->default(0);
+            $table->boolean('delivered')->default(0);
+            $table->unsignedInteger('opens')->default(0);
+            $table->unsignedInteger('clicks')->default(0);
+            $table->unsignedInteger('unsubscribes')->default(0);
+            $table->unsignedInteger('bounces')->default(0);
+            $table->unsignedInteger('complaints')->default(0);
+            $table->string('from_name');
+            $table->string('from_email');
+            $table->string('type')->nullable();
             $table->foreignId('list_id')->constrained('email_lists')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
